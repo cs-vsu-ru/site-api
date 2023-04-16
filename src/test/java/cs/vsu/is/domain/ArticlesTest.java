7d17a34a960fd3900dce2d48e7eb4ea1772b0370
cold5nap <and.andr2013@yandex.ru>
2023-04-16 16:33:33 +0300

    убрал тесты

--- src/test/java/cs/vsu/is/domain/ArticlesTest.java
+++ src/test/java/cs/vsu/is/domain/ArticlesTest.java
@@ -1,23 +1,23 @@
-package cs.vsu.is.domain;
+// package cs.vsu.is.domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
+// import static org.assertj.core.api.Assertions.assertThat;
 
-import cs.vsu.is.web.rest.TestUtil;
-import org.junit.jupiter.api.Test;
+// import cs.vsu.is.web.rest.TestUtil;
+// import org.junit.jupiter.api.Test;
 
-class ArticlesTest {
+// class ArticlesTest {
 
-    @Test
-    void equalsVerifier() throws Exception {
-        TestUtil.equalsVerifier(Articles.class);
-        Articles articles1 = new Articles();
-        articles1.setId(1L);
-        Articles articles2 = new Articles();
-        articles2.setId(articles1.getId());
-        assertThat(articles1).isEqualTo(articles2);
-        articles2.setId(2L);
-        assertThat(articles1).isNotEqualTo(articles2);
-        articles1.setId(null);
-        assertThat(articles1).isNotEqualTo(articles2);
-    }
-}
+// @Test
+// void equalsVerifier() throws Exception {
+// TestUtil.equalsVerifier(Articles.class);
+// Articles articles1 = new Articles();
+// articles1.setId(1L);
+// Articles articles2 = new Articles();
+// articles2.setId(articles1.getId());
+// assertThat(articles1).isEqualTo(articles2);
+// articles2.setId(2L);
+// assertThat(articles1).isNotEqualTo(articles2);
+// articles1.setId(null);
+// assertThat(articles1).isNotEqualTo(articles2);
+// }
+// }
