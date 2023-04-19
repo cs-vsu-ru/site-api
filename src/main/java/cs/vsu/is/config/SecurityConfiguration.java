8cc9e67fe403f75b21544d68b4b76749b48391b8
vithar <roman.anokhin@combridge-itc.de>
2023-04-19 11:21:46 +0300

    fix

--- src/main/java/cs/vsu/is/config/SecurityConfiguration.java
+++ src/main/java/cs/vsu/is/config/SecurityConfiguration.java
@@ -65,6 +65,7 @@ public class SecurityConfiguration {
             .antMatchers("/swagger-ui/**").permitAll()
             .antMatchers("/test/**").permitAll()
             .antMatchers("/api/authenticate").permitAll()
+            .antMatchers("/api/employess").permitAll()
             .antMatchers("/api/register").permitAll()
             .antMatchers("/api/activate").permitAll()
             .antMatchers("/api/account/reset-password/init").permitAll()
