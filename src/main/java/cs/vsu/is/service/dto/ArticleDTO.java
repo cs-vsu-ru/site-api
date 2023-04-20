9b2443f32928bc7b95f3f138782c3c9eb2dcfdfb
cold5nap <and.andr2013@yandex.ru>
2023-04-20 17:50:03 +0300

    Добавил в дто недостающие поля

--- src/main/java/cs/vsu/is/service/dto/ArticleDTO.java
+++ src/main/java/cs/vsu/is/service/dto/ArticleDTO.java
@@ -19,6 +19,8 @@ public class ArticleDTO {
   private String publicationDate;
   private String publicationTime;
   private String content;
+  private String title;
+  private String imageURL;
   // private EmployeeDTO employee;
   private AccessModeDTO accessModes;
 
