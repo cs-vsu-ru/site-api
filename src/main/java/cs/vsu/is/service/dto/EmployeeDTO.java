677cd1b7c9b075447b888a5a6a3750837832b31e
vithar <roman.anokhin@combridge-itc.de>
2023-04-23 16:29:46 +0300

    Добавил главную роль в api/account GET

--- src/main/java/cs/vsu/is/service/dto/EmployeeDTO.java
+++ src/main/java/cs/vsu/is/service/dto/EmployeeDTO.java
@@ -19,20 +19,20 @@ import lombok.Setter;
 @SuppressWarnings("common-java:DuplicatedBlocks")
 public class EmployeeDTO {
 
-  private Long id;
-  private String patronymic;
-  private String post;
-  private String academicTitle;
-  private String academicDegree;
-  private String experience;
-  private String professionalExperience;
-  private LocalDate dateOfBirth;
-  private AdminUserDTO user;
-  private Set<ArticleDTO> articles = new HashSet<>();
-  private Set<EventDTO> events = new HashSet<>();
-  private Set<LessonDTO> lessons = new HashSet<>();
-  private Set<PageDTO> pages = new HashSet<>();
-  private Set<ScientificLeadershipsDTO> scientificLeaderships = new HashSet<>();
-  private Set<TeachingDTO> teachings = new HashSet<>();
-
+    private Long id;
+    private String patronymic;
+    private String post;
+    private String academicTitle;
+    private String academicDegree;
+    private String experience;
+    private String professionalExperience;
+    private LocalDate dateOfBirth;
+    private AdminUserDTO user;
+    private Set<ArticleDTO> articles = new HashSet<>();
+    private Set<EventDTO> events = new HashSet<>();
+    private Set<LessonDTO> lessons = new HashSet<>();
+    private Set<PageDTO> pages = new HashSet<>();
+    private Set<ScientificLeadershipsDTO> scientificLeaderships = new HashSet<>();
+    private Set<TeachingDTO> teachings = new HashSet<>();
+    private String mainRole;
 }
