b7e9499ba4110b2450b9a1a259fa846a3622978f
Элина Обухова <45429218+WriteWrote@users.noreply.github.com>
2023-04-24 16:51:12 +0300

    after fetching

--- src/main/java/cs/vsu/is/web/rest/UtilResource.java
+++ src/main/java/cs/vsu/is/web/rest/UtilResource.java
@@ -1,12 +1,16 @@
 package cs.vsu.is.web.rest;
 
 import cs.vsu.is.security.AuthoritiesConstants;
+import cs.vsu.is.service.ParserService;
 import cs.vsu.is.service.dto.UploadFileDTO;
 import lombok.RequiredArgsConstructor;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jca.work.WorkManagerTaskExecutor;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -21,6 +25,8 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class UtilResource {
     private final Logger log = LoggerFactory.getLogger(UtilResource.class);
+    private final String timetableExtension = ".xlsx";
+    private final ParserService parserService;
 
     @Value("${domain}")
     private String domain;
@@ -31,10 +37,17 @@ public class UtilResource {
         try {
             byte[] bytes = file.getBytes();
             UUID uuid = UUID.randomUUID();
-            Path path = Path.of("files/"+uuid + file.getOriginalFilename());
+            Path path = Path.of("files/" + uuid + file.getOriginalFilename());
             log.debug("path {}", path);
             Path write = Files.write(path, bytes);
-            return ResponseEntity.ok().body(domain+"api/" + write);
+
+            if (file.getName().endsWith(timetableExtension)) {
+                Workbook workbook = new XSSFWorkbook(path.toString());
+                parserService.parseXLSXToSlots(workbook, 0);
+                parserService.parseXLSXToSlots(workbook, 1);
+            }
+
+            return ResponseEntity.ok().body(domain + "api/" + write);
         } catch (IOException e) {
             e.printStackTrace();
             return ResponseEntity.badRequest().body(null);
