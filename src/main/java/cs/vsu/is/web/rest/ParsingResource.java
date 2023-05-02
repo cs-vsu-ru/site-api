221add0d6a7f4fe6f2d669e8cc1e6600b276f7d9
Элина Обухова <45429218+WriteWrote@users.noreply.github.com>
2023-05-02 21:16:28 +0300

    added plan for developing algorithm

--- src/main/java/cs/vsu/is/web/rest/ParsingResource.java
+++ src/main/java/cs/vsu/is/web/rest/ParsingResource.java
@@ -2,16 +2,16 @@ package cs.vsu.is.web.rest;
 
 import cs.vsu.is.service.ParserService;
 import lombok.RequiredArgsConstructor;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.streaming.SXSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -27,7 +27,7 @@ public class ParsingResource {
     public ResponseEntity<String> parseFile(
         @RequestParam("filepath") String filepath) {
         try {
-            String path = filepath.substring(filepath.lastIndexOf('/'));
+            String path = filepath.substring(filepath.lastIndexOf('\\'));
             Workbook workbook = new XSSFWorkbook("files" + path);
             parserService.parseXLSXToSlots(workbook, 0);
             parserService.parseXLSXToSlots(workbook, 1);
@@ -37,4 +37,29 @@ public class ParsingResource {
             return ResponseEntity.badRequest().body("Parsing failed with reason: " + e.getMessage());
         }
     }
+
+    @GetMapping("/filterTimetable")
+    public ResponseEntity<String> filterTimetable(@RequestParam String teacherName) {
+        try {
+            String html = "";
+
+//            Workbook workbook = new XSSFWorkbook();
+//            Sheet sheet = new XSSFSheet();
+//            html = sheet.toString();
+
+            //todo: algorithm of sorting timetable
+            // 1. search for teacher name
+            // 2. search for all lessons with id of previously found teacher
+            // 3. sort timetables by weekdays and by time
+            // 4. form a new Workbook and fill it with content
+            // 5. solve problem with parsing XSSFWorkbook to html
+
+            return ResponseEntity.ok().body(domain + "api/filterTimetable\n" + html);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().body("Filtering timetable failed. Reason: " + e.getMessage());
+        }
+    }
+
+
 }
