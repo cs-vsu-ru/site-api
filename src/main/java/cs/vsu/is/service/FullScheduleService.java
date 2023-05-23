4909d75ec3dc5654468a11eadf4054949f3e7172
vithar <roman.anokhin@combridge-itc.de>
2023-05-23 22:06:46 +0300

    добавил создание пустых блоков в расписании

--- src/main/java/cs/vsu/is/service/FullScheduleService.java
+++ src/main/java/cs/vsu/is/service/FullScheduleService.java
@@ -1,8 +1,11 @@
 package cs.vsu.is.service;
 
+import cs.vsu.is.domain.EduSchedulePlace;
 import cs.vsu.is.domain.Employee;
 import cs.vsu.is.domain.Lesson;
+import cs.vsu.is.repository.EduSchedulePlaceRepository;
 import cs.vsu.is.repository.EmployeeRepository;
+import cs.vsu.is.repository.LessonRepository;
 import cs.vsu.is.service.dto.fullschedule.EmployeeScheduleDTO;
 import cs.vsu.is.service.dto.fullschedule.FullLessonDTO;
 import cs.vsu.is.service.dto.fullschedule.FullScheduleDTO;
@@ -14,9 +17,13 @@ import java.util.stream.Collectors;
 @Service
 public class FullScheduleService {
     private final EmployeeRepository employeeRepository;
+    private final LessonRepository lessonRepository;
+    private final EduSchedulePlaceRepository eduSchedulePlaceRepository;
 
-    public FullScheduleService(EmployeeRepository employeeRepository) {
+    public FullScheduleService(EmployeeRepository employeeRepository, LessonRepository lessonRepository, EduSchedulePlaceRepository eduSchedulePlaceRepository) {
         this.employeeRepository = employeeRepository;
+        this.lessonRepository = lessonRepository;
+        this.eduSchedulePlaceRepository = eduSchedulePlaceRepository;
     }
 
     public List<EmployeeScheduleDTO> getFullSchedule() {
@@ -27,12 +34,12 @@ public class FullScheduleService {
             List<String> times = Arrays.asList("8:00", "9:45", "11:30", "13:25", "15:10", "16:55", "18:40", "20:10");
             List<FullScheduleDTO> fullScheduleDTOS = new LinkedList<>();
             for (String time : times) {
-                FullLessonDTO monday = getLessonForDayOfWeekAndTime(employeeLessons, time, 0);
-                FullLessonDTO tuesday = getLessonForDayOfWeekAndTime(employeeLessons, time, 1);
-                FullLessonDTO wednesday = getLessonForDayOfWeekAndTime(employeeLessons, time, 2);
-                FullLessonDTO thursday = getLessonForDayOfWeekAndTime(employeeLessons, time, 3);
-                FullLessonDTO friday = getLessonForDayOfWeekAndTime(employeeLessons, time, 4);
-                FullLessonDTO saturday = getLessonForDayOfWeekAndTime(employeeLessons, time, 5);
+                FullLessonDTO monday = getLessonForDayOfWeekAndTime(employeeLessons, time, 0, employee);
+                FullLessonDTO tuesday = getLessonForDayOfWeekAndTime(employeeLessons, time, 1, employee);
+                FullLessonDTO wednesday = getLessonForDayOfWeekAndTime(employeeLessons, time, 2, employee);
+                FullLessonDTO thursday = getLessonForDayOfWeekAndTime(employeeLessons, time, 3, employee);
+                FullLessonDTO friday = getLessonForDayOfWeekAndTime(employeeLessons, time, 4, employee);
+                FullLessonDTO saturday = getLessonForDayOfWeekAndTime(employeeLessons, time, 5, employee);
                 FullScheduleDTO fullScheduleDTO = new FullScheduleDTO();
                 fullScheduleDTO.setTime(time);
                 fullScheduleDTO.setMonday(monday);
@@ -64,17 +71,16 @@ public class FullScheduleService {
             return null;
         }
         Employee employee = byId.get();
-        List<EmployeeScheduleDTO> employeeScheduleDTOS = new LinkedList<>();
         Set<Lesson> employeeLessons = employee.getLessons();
         List<String> times = Arrays.asList("8:00", "9:45", "11:30", "13:25", "15:10", "16:55", "18:40", "20:10");
         List<FullScheduleDTO> fullScheduleDTOS = new LinkedList<>();
         for (String time : times) {
-            FullLessonDTO monday = getLessonForDayOfWeekAndTime(employeeLessons, time, 0);
-            FullLessonDTO tuesday = getLessonForDayOfWeekAndTime(employeeLessons, time, 1);
-            FullLessonDTO wednesday = getLessonForDayOfWeekAndTime(employeeLessons, time, 2);
-            FullLessonDTO thursday = getLessonForDayOfWeekAndTime(employeeLessons, time, 3);
-            FullLessonDTO friday = getLessonForDayOfWeekAndTime(employeeLessons, time, 4);
-            FullLessonDTO saturday = getLessonForDayOfWeekAndTime(employeeLessons, time, 5);
+            FullLessonDTO monday = getLessonForDayOfWeekAndTime(employeeLessons, time, 0, employee);
+            FullLessonDTO tuesday = getLessonForDayOfWeekAndTime(employeeLessons, time, 1, employee);
+            FullLessonDTO wednesday = getLessonForDayOfWeekAndTime(employeeLessons, time, 2, employee);
+            FullLessonDTO thursday = getLessonForDayOfWeekAndTime(employeeLessons, time, 3, employee);
+            FullLessonDTO friday = getLessonForDayOfWeekAndTime(employeeLessons, time, 4, employee);
+            FullLessonDTO saturday = getLessonForDayOfWeekAndTime(employeeLessons, time, 5, employee);
             FullScheduleDTO fullScheduleDTO = new FullScheduleDTO();
             fullScheduleDTO.setTime(time);
             fullScheduleDTO.setMonday(monday);
@@ -98,7 +104,7 @@ public class FullScheduleService {
         return employeeScheduleDTO;
     }
 
-    private FullLessonDTO getLessonForDayOfWeekAndTime(Set<Lesson> lessons, String startTime, int weekNumber) {
+    private FullLessonDTO getLessonForDayOfWeekAndTime(Set<Lesson> lessons, String startTime, int weekNumber, Employee employee) {
         if (lessons == null || lessons.isEmpty()) {
             return new FullLessonDTO();
         }
@@ -107,12 +113,26 @@ public class FullScheduleService {
             .filter(lesson -> (lesson.getEduSchedulePlace().getDayOfWeak() == weekNumber && lesson.getEduSchedulePlace().getStartTime().equals(startTime)))
             .collect(Collectors.toList());
         if(collect.isEmpty()) {
-            return new FullLessonDTO();
+            EduSchedulePlace eduSchedulePlace = new EduSchedulePlace();
+            eduSchedulePlace.setDayOfWeak(weekNumber);
+            eduSchedulePlace.setStartTime(startTime);
+            eduSchedulePlace = eduSchedulePlaceRepository.save(eduSchedulePlace);
+            Lesson lesson = new Lesson();
+            lesson.setEduSchedulePlace(eduSchedulePlace);
+            lesson.setEmployee(employee);
+            lesson = lessonRepository.save(lesson);
+            FullLessonDTO fullLessonDTO = new FullLessonDTO();
+            fullLessonDTO.setLessonId(lesson.getId());
+            return fullLessonDTO;
         }
         FullLessonDTO fullLessonDTO = new FullLessonDTO();
         fullLessonDTO.setPlacement(collect.get(0).getClassroom());
         fullLessonDTO.setCourse(collect.get(0).getCourse());
-        fullLessonDTO.setGroup(collect.get(0).getGroup().toString());
+        if (collect.get(0).getGroup() != null) {
+            fullLessonDTO.setGroup(collect.get(0).getGroup().toString());
+        } else {
+            fullLessonDTO.setGroup(null);
+        }
         fullLessonDTO.setLessonId(collect.get(0).getId());
         fullLessonDTO.setLesson(collect.get(0).getSubjectName());
         fullLessonDTO.setIsDenominator(collect.get(0).getEduSchedulePlace().getIsDenominator());
