11788f5205f850ca1ea33fa79775d6a16be10dc3
vithar <roman.anokhin@combridge-itc.de>
2023-05-25 01:44:34 +0300

    добавил числитель и знаменатель в json

--- src/main/java/cs/vsu/is/service/dto/fullschedule/FullScheduleDTO.java
+++ src/main/java/cs/vsu/is/service/dto/fullschedule/FullScheduleDTO.java
@@ -3,14 +3,16 @@ package cs.vsu.is.service.dto.fullschedule;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class FullScheduleDTO {
     private String time;
-    private FullLessonDTO monday;
-    private FullLessonDTO tuesday;
-    private FullLessonDTO wednesday;
-    private FullLessonDTO thursday;
-    private FullLessonDTO friday;
-    private FullLessonDTO saturday;
+    private List<FullLessonDTO> monday;
+    private List<FullLessonDTO> tuesday;
+    private List<FullLessonDTO> wednesday;
+    private List<FullLessonDTO> thursday;
+    private List<FullLessonDTO> friday;
+    private List<FullLessonDTO> saturday;
 }
