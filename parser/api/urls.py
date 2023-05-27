e7320572ccd22a1778ccb96279125de359eb634e
GentleEnvy <komarov.sergei163@gmail.com>
2023-05-27 18:31:02 +0300

    fix parser routing

--- parser/api/urls.py
+++ parser/api/urls.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
@@ -6,11 +5,9 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = []
 
 urlpatterns += [
-    path('is/__docs__/', SpectacularAPIView.as_view(), name='__docs__'),
-    path('', SpectacularSwaggerView.as_view(url_name='__docs__')),
-    path('admin/', admin.site.urls),
-    path('base/', include('app.base.urls')),
+    path('is/api/parser/__docs__/', SpectacularAPIView.as_view(), name='__docs__'),
+    path('api/parser/__docs__/', SpectacularAPIView.as_view()),
+    path('api/parser/', SpectacularSwaggerView.as_view(url_name='__docs__')),
+    path('api/parser/admin/', admin.site.urls),
+    path('api/parser/base/', include('app.base.urls')),
 ]
-
-if settings.URL_PREFIX is not None:
-    urlpatterns = [path(f"{settings.URL_PREFIX}/", include(urlpatterns))]
