0c4f980958efff77465c1fff169802d4e604456f
vithar <roman.anokhin@combridge-itc.de>
2023-05-27 23:58:57 +0300

    isd-48 вывод мероприятий

--- src/main/java/cs/vsu/is/domain/Events.java
+++ src/main/java/cs/vsu/is/domain/Events.java
@@ -20,50 +20,57 @@ import javax.persistence.*;
 @SuppressWarnings("common-java:DuplicatedBlocks")
 public class Events implements Serializable {
 
-  private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "id")
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
 
-  @Column(name = "publication_date")
-  private String publicationDate;
+    @Column(name = "publication_date")
+    private String publicationDate;
 
-  @Column(name = "publication_time")
-  private String publicationTime;
+    @Column(name = "publication_time")
+    private String publicationTime;
 
-  @Column(name = "content", columnDefinition = "json")
-  private String content;
+    @Column(name = "content", columnDefinition = "json")
+    private String content;
 
-  @Column(name = "last_modified_date")
-  private String lastModifiedDate;
+    @Column(name = "last_modified_date")
+    private String lastModifiedDate;
 
-  @Column(name = "last_modified_time")
-  private String lastModifiedTime;
+    @Column(name = "last_modified_time")
+    private String lastModifiedTime;
 
-  @Column(name = "start_time")
-  private String startTime;
+    @Column(name = "start_time")
+    private String startTime;
 
-  @Column(name = "start_date")
-  private String startDate;
+    @Column(name = "start_date")
+    private String startDate;
 
-  @Column(name = "end_time")
-  private String endTime;
+    @Column(name = "end_time")
+    private String endTime;
 
-  @Column(name = "end_date")
-  private String endDate;
+    @Column(name = "end_date")
+    private String endDate;
 
-  @Column(name = "title")
-  private String title;
+    @Column(name = "title")
+    private String title;
 
-  @ManyToOne
-  @JsonIgnoreProperties(value = { "user", "articles", "events", "lessons", "pages", "scientificLeaderships",
-      "teachings", "roles" }, allowSetters = true)
-  private Employee employee;
+    @Column(nullable = false)
+    private LocalDateTime timestamp;
 
-  @ManyToOne
-  @JsonIgnoreProperties(value = { "articles", "events", "pages" }, allowSetters = true)
-  private AccessModes accessModes;
+    @ManyToOne
+    @JsonIgnoreProperties(value = {"user", "articles", "events", "lessons", "pages", "scientificLeaderships",
+        "teachings", "roles"}, allowSetters = true)
+    private Employee employee;
 
+    @ManyToOne
+    @JsonIgnoreProperties(value = {"articles", "events", "pages"}, allowSetters = true)
+    private AccessModes accessModes;
+
+    @PrePersist
+    public void prePersist() {
+        this.timestamp = LocalDateTime.now();
+    }
 }
