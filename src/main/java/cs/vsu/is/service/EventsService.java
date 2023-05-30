7a296df3509506df710d6dfa34cc78c17a40caf1
vithar <roman.anokhin@combridge-itc.de>
2023-05-30 13:24:10 +0300

    news date sorting

--- src/main/java/cs/vsu/is/service/EventsService.java
+++ src/main/java/cs/vsu/is/service/EventsService.java
@@ -7,6 +7,8 @@ import cs.vsu.is.service.convertor.EventConverter;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
@@ -92,9 +94,50 @@ public class EventsService {
    */
   @Transactional(readOnly = true)
   public List<EventDTO> findAll() {
-    log.debug("Request to get all Events");
-    return eventsRepository.findAllOrderByTimestampDesc().stream().map(eventsMapper::toDto)
-        .collect(Collectors.toCollection(LinkedList::new));
+      log.debug("Request to get all Events");
+      DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+      return eventsRepository.findAll().stream()
+          .sorted((o1, o2) -> {
+              LocalDateTime now = LocalDateTime.now();
+              LocalDateTime date1 = now.minusDays(1000);
+              LocalDateTime date2 = now.minusDays(1000);
+              boolean isDate1Valid = true;
+              boolean isDate2Valid = true;
+
+              try {
+                  date1 = LocalDateTime.parse(o1.getStartDate() + " " + o1.getStartTime(), FORMATTER);
+              } catch (Exception ignored) {
+                  isDate1Valid = false;
+              }
+              try {
+                  date2 = LocalDateTime.parse(o2.getStartDate() + " " + o2.getStartTime(), FORMATTER);
+              } catch (Exception ignored) {
+                  isDate2Valid = false;
+              }
+
+              if (isDate1Valid && isDate2Valid) {
+                  if (date1.isBefore(now) && date2.isBefore(now)) {
+                      return date1.compareTo(date2);
+                  } else if (date1.isBefore(now)) {
+                      return 1; // date1 is expired, place it after date2
+                  } else if (date2.isBefore(now)) {
+                      return -1; // date2 is expired, place it after date1
+                  } else {
+                      // Calculate the difference between dates and sort in ascending order
+                      long date1Difference = ChronoUnit.SECONDS.between(now, date1);
+                      long date2Difference = ChronoUnit.SECONDS.between(now, date2);
+                      return Long.compare(date1Difference, date2Difference);
+                  }
+              } else if (isDate1Valid) {
+                  return -1; // date1 is a valid date, place it before date2
+              } else if (isDate2Valid) {
+                  return 1; // date2 is a valid date, place it before date1
+              } else {
+                  return 0; // both are non-date strings, maintain the current order
+              }
+          })
+          .map(eventsMapper::toDto)
+          .collect(Collectors.toCollection(LinkedList::new));
   }
 
   /**
