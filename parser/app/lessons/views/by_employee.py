306e14423faf8faf222a3d70daf1c5e523036d13
GentleEnvy <komarov.sergei163@gmail.com>
2023-05-30 18:10:17 +0300

    add page cache

--- parser/app/lessons/views/by_employee.py
+++ parser/app/lessons/views/by_employee.py
@@ -1,3 +1,6 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+
 from app.base.views.base import BaseView
 from app.employees.models import Employee
 from app.lessons.serializers.by_employee import GET_LessonsByEmployeeSerializer
@@ -8,6 +11,7 @@ class LessonsByEmployeeView(BaseView):
     queryset = Employee.objects.all()
     lookup_field = 'id'
     lookup_url_kwarg = 'employee_id'
-    
+
+    @method_decorator(cache_page(60))
     def get(self):
         return self.retrieve()
--- parser/app/lessons/views/general.py
+++ parser/app/lessons/views/general.py
@@ -1,3 +1,5 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from rest_framework.response import Response
 
 from app.base.views.base import BaseView
@@ -10,6 +12,7 @@ class LessonsView(BaseView):
     queryset = Employee.objects.order_by('name')
     pagination_class = None
 
+    @method_decorator(cache_page(60))
     def get(self):
         serializer = self.get_serializer({'employees': self.get_filtered_queryset()})
         return Response(serializer.data)
